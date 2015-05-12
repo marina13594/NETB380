83dd3bfed00209f97957ed972d1b164a2fc0bb89
Fenigor <starblocer@abv.bg>
2015-05-12 21:46:10 +0300

    Addin the tree and list view to the add button

--- secdialog.cpp
+++ secdialog.cpp
@@ -6,9 +6,27 @@ SecDialog::SecDialog(QWidget *parent) :
     ui(new Ui::SecDialog)
 {
     ui->setupUi(this);
+
+    QString sPath = "C:/";
+
+    dirmodel = new QFileSystemModel(this);
+    dirmodel->setRootPath(sPath);
+    dirmodel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    ui->treeView->setModel(dirmodel);
+
+    filemodel = new QFileSystemModel(this);
+    filemodel->setFilter(QDir::Files);
+    filemodel->setRootPath(sPath);
+    ui->listView->setModel(filemodel);
 }
 
 SecDialog::~SecDialog()
 {
     delete ui;
+}
+
+void SecDialog::on_treeView_clicked(const QModelIndex &index)
+{
+    QString sPath = dirmodel->fileInfo(index).absoluteFilePath();
+    ui->listView->setRootIndex(filemodel->setRootPath(sPath));
 }
--- secdialog.h
+++ secdialog.h
@@ -2,6 +2,7 @@
 #define SECDIALOG_H
 
 #include <QDialog>
+#include <QFileSystemModel>
 
 namespace Ui {
 class SecDialog;
@@ -15,8 +16,13 @@ public:
     explicit SecDialog(QWidget *parent = 0);
     ~SecDialog();
 
+private slots:
+    void on_treeView_clicked(const QModelIndex &index);
+
 private:
     Ui::SecDialog *ui;
+    QFileSystemModel *dirmodel;
+    QFileSystemModel *filemodel;
 };
 
 #endif // SECDIALOG_H
